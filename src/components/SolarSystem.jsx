40301a30327a8b2dec3ec66dd610651f3c6061e0
Otavio Vinicius <viinident@gmail.com>
2022-07-20 15:27:10 -0300

    Cria componente SolarSystem

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return <div data-testid="solar-system">consegui</div>;
+  }
+}
+
+export default SolarSystem;
