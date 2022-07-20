aad6024c0dd14d6134fdafac42ba8f22b4eac1eb
Otavio Vinicius <viinident@gmail.com>
2022-07-20 20:05:55 -0300

    Requisito 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
+// import Planets from '../data/planets';
+
+const planetList = planets.map((planet, index) => {
+  const { name, image } = planet;
+  return (
+    <li key={ index }>
+      <PlanetCard planetName={ name } planetImage={ image } />
+    </li>
+  );
+});
 
 class SolarSystem extends Component {
   render() {
-    return <div data-testid="solar-system"><Title headline="Planetas" /></div>;
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        <ul>
+          {planetList}
+        </ul>
+      </div>
+    );
   }
 }
 
