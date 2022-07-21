5bca7a672ce4069a3dac271f7440fe4e414e01e2
Otavio Vinicius <viinident@gmail.com>
2022-07-20 22:11:46 -0300

    Desafio 9

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import MissionCard from './MissionCard';
 import Title from './Title';
+import missions from '../data/missions';
+
+const listMissions = missions.map((mission, key) => {
+  const { name, year, country, destination } = mission;
+  const n = name;
+  const y = year;
+  const c = country;
+  const d = destination;
+
+  return (
+    <MissionCard key={ key } name={ n } year={ y } country={ c } destination={ d } />
+  );
+});
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        { listMissions }
       </div>
     );
   }
